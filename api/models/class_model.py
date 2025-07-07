73c72f52cd4499e1df37b947f785fd4429834e60
ManuelCarreiras <71514523+ManuelCarreiras@users.noreply.github.com>
2025-07-07 22:57:28 +0100

    class model adjusted

--- api/models/class_model.py
+++ api/models/class_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import JSON
 
-class Class(db.Model):
+class ClassModel(db.Model):
     __tablename__ = 'class'
     _id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     class_name = db.Column(db.String(100), nullable=False)
