25a5b3f627adeee1918d204cd141f4c7d6f80298
ManuelCarreiras <71514523+ManuelCarreiras@users.noreply.github.com>
2025-12-16 23:25:04 +0000

    clean useless scripts

--- api/migrations/remove_weight_from_assignment.sql
+++
@@ -1,8 +0,0 @@
--- Remove weight column from assignment table
--- Weight is now defined at the component level in grading_criteria, not per assignment
-
-ALTER TABLE assignment DROP COLUMN IF EXISTS weight;
-
--- Comment update
-COMMENT ON TABLE assignment IS 'Individual assignments - graded equally within their assessment type (Test, Homework, etc.)';
-
--- api/migrations/remove_weight_from_student_year_grade.sql
+++
@@ -1,5 +0,0 @@
--- Remove total_weight_graded from student_year_grade
--- No longer needed since assignments don't have weights
-
-ALTER TABLE student_year_grade DROP COLUMN IF EXISTS total_weight_graded;
-
