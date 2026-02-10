b7e15462ae65d345e369721c97a982290abee6da
ManuelCarreiras <71514523+ManuelCarreiras@users.noreply.github.com>
2026-02-10 01:47:26 +0000

    dev env w/o ssl; solved workers racing condition

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 enviroment=dev
-export DOPPLER_TOKEN_TEMP="${DOPPLER_TOKEN:-$(doppler configs tokens create temp_dev --project moz_school --config dev --plain --max-age 20m)}"
+export DOPPLER_TOKEN="${DOPPLER_TOKEN:-$(doppler configs tokens create temp_dev --project moz_school --config dev --plain --max-age 20m)}"
 
 doppler run -- docker compose build --parallel && \
 doppler run -- docker compose up
